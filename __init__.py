8954e09517dd85f24f1665c5430e33b6484f2cf4
bewegende Architektur e.U <43149649+bewegende-Architektur@users.noreply.github.com>
2023-03-08 19:29:44 +0100

    prepare vor Version 0.1.5

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Phänotyp",
     "description": "Genetic optimization of architectural structures",
     "author": "bewegende Architektur e.U. and Karl Deix",
-    "version": (0, 1, 4),
+    "version": (0, 1, 5),
     "blender": (3, 4, 1),
     "location": "3D View > Tools",
 }
